5b42461ffa74dd39e5fabfa95479adc95703cc39
stel723 <ekaterinaoyyyy@yandex.ru>
2025-06-24 20:57:41 +0300

    project

--- my-projectt/resources/views/layouts/app.blade.php
+++ my-projectt/resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
-    <title>Интернет-магазин</title>
+    <title>Пасьянс</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -9,4 +9,4 @@
         @yield('content')
     </div>
 </body>
-</html>
+</html> -->
--- my-projectt/routes/web.php
+++ my-projectt/routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('resources/views/pages/home.blade.php');
 });
