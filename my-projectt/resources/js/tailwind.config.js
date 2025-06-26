e961d5dc3811a398d9bb2397c5949ba9b38816f7
stel723 <ekaterinaoyyyy@yandex.ru>
2025-06-26 20:56:46 +0300

    project

--- my-projectt/resources/js/tailwind.config.js
+++ my-projectt/resources/js/tailwind.config.js
@@ -1,29 +1,12 @@
-@import 'tailwindcss';
-
-@source '../../vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php';
-@source '../../storage/framework/views/*.php';
-@source '../**/*.blade.php';
-@source '../**/*.js';
-
-@theme {
-    --font-sans: 'Instrument Sans', ui-sans-serif, system-ui, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
-        'Segoe UI Symbol', 'Noto Color Emoji';
-}
-
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '../../vendor/laravel/framework/src/Illuminate/Pagination/resources/views/**/*.blade.php',
-    '../../storage/framework/views/**/*.php',
-    '../**/*.blade.php',
-    '../**/*.js',
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Instrument Sans', 'ui-sans-serif', 'system-ui', 'sans-serif', 
-               'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
-      },
-    },
+    extend: {},
   },
+  plugins: [],
 }
--- my-projectt/resources/views/layouts/app.blade.php
+++ my-projectt/resources/views/layouts/app.blade.php
@@ -1,12 +1,12 @@
-<!-- <!DOCTYPE html>
-<html>
+<!DOCTYPE html>
+<html lang="ru">
 <head>
-    <title>Пасьянс</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Мой проект</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <div class="container">
-        @yield('content')
-    </div>
+    <!-- ваш контент -->
 </body>
-</html> -->
+</html>
